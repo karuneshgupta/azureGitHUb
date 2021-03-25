f8138f64833ec6e4a8df0696260f3ff546be6c1e
karuneshgupta <42061618+karuneshgupta@users.noreply.github.com>
2021-03-25 12:11:26 +0530

    main branch
    hello world

---
+++ Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+  public static void main(String[]args)
+  {
+   // System.out.println("hello there ");
+
+    int myfirstNumber =(10+5)+(2*10);
+    int mySecondNumber = 12;
+    int mythirdNumber = myfirstNumber*2 ;
+
+    int myTotal = myfirstNumber+mySecondNumber+mythirdNumber;
+    int myLastOne= 1000 -myTotal;
+    System.out.println(myTotal);
+    System.out.println(myLastOne);
+
+
+  }
+}
